5812a20fadb1d234dd885bf6590294bd35289d84
pablo806 <pablo.gonzalez0m@gmail.com>
2024-11-18 23:22:10 +0100

    Create script.js

---
+++ script.js
@@ -0,0 +1,9 @@
+// JavaScript pour le défilement fluide
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function(e) {
+        e.preventDefault();
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
+});
